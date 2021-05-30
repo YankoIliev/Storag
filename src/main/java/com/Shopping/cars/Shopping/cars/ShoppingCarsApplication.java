c4338d67102006013beca975723fa6a02e499e38
YankoIliev <you@example.com>
2021-05-30 16:51:40 +0300

    initial comit

---
+++ src/main/java/com/Shopping/cars/Shopping/cars/ShoppingCarsApplication.java
@@ -0,0 +1,13 @@
+package com.Shopping.cars.Shopping.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shopping/cars/Shopping/cars/ShoppingCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shopping.cars.Shopping.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
